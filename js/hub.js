1bb05ca67189a89e6baa170eac9eab224abe3c1c
Calvin John <calvindegr8@gmail.com>
2021-10-15 06:52:05 +0400

    creating an initial build

---
+++ js/hub.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $("#DECO3801").click(function(event) {
+        alert("This page is currently unavailable.");  
+		    event.preventDefault();
+    });
+    $("#CSSE3003").click(function(event) {
+        alert("This page is currently unavailable.");  
+		    event.preventDefault();
+    });
+    $("#INFS1200").click(function(event) {
+        alert("This page is currently unavailable.");  
+		    event.preventDefault();
+    });    
+    $("#lecture").click(function(event) {
+        alert("This page is currently unavailable.");  
+		    event.preventDefault();
+    });
+    $("#blackboard").click(function(event) {
+        alert("This page is currently unavailable.");  
+		    event.preventDefault();
+    });
+    $("#fullTable").click(function(event) {
+        alert("This table is full. Please select a different table.");  
+		    event.preventDefault();
+    });
+});
